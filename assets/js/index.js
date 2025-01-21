cd8308c338bf0b40458c5a4d9ffddb05d8a249d5
Zx Lim <limgoodluck@gmail.com>
2025-01-21 19:13:36 +0800

    resolved merging issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const apikey=""
 
 // const endpoint = `https://api.openweathermap.org/data/3.0/onecall?lat=1.352&lon=103.81&appid=${apikey}`
@@ -44,23 +43,3 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 
-=======
-const apikey=""
-
-const endpoint = `https://api.openweathermap.org/data/3.0/onecall?lat=1.352&lon=103.81&appid=${apikey}`
-
-fetch(endpoint)
-.then((response) => {
-	if (!response.ok) {
-		throw new Error(`${response.status}`)
-	}
-	return response.json()
-})
-.then ((data) => {
-	console.log(data.timezone_offset);
-	console.log(data.daily);
-})
-.catch ((error) => {
-	console.error(error);
-})
->>>>>>> main
